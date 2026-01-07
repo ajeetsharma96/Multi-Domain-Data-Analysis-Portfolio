0486352f1863323c6c68dc2256e402b9802aa2a2
ajeetsharma96 <ajeetsharma500045@gmail.com>
2026-01-07 22:34:57 +0530

    written code for covid-19 analysis

---
+++ covid.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("covid_data.csv")
+df['Date'] = pd.to_datetime(df['Date'])
+
+plt.plot(df['Date'], df['Confirmed'], label='Confirmed')
+plt.plot(df['Date'], df['Recovered'], label='Recovered')
+plt.plot(df['Date'], df['Deaths'], label='Deaths')
+plt.legend()
+plt.title("COVID-19 Trends")
+plt.xlabel("Date")
+plt.ylabel("Number of Cases")
+plt.show()
